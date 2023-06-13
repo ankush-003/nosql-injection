6108013f0b6b378b7bc4ebab3abdc68b0bac50e1
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2023-06-14 00:31:47 +0530

    added references

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nosql-injection
 a simple noSQL injection sample
+
+## References:
+- [NoSQL Injection OWASP](https://owasp.org/www-project-web-security-testing-guide/latest/4-Web_Application_Security_Testing/07-Input_Validation_Testing/05.6-Testing_for_NoSQL_Injection)
+- [Hacktricks](https://book.hacktricks.xyz/pentesting-web/nosql-injection)
--- test.mjs
+++ test.mjs
@@ -6,8 +6,10 @@ let res = fetch('http://localhost:3000/user', {
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({
+        // email: 'ankush4@pes.com',
+        // password: '$2a$10$6AqJQwOpPHvP7oz2Nk27uet.t5Ihw.0lVh497AXkr.Mik/3QIDNze'
         email: 'ankush',
-        password: 'gan'
+        password: 'ganya'
     })
 }).then((res) => {
     return res.json();
