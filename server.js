71c99f8ce0adc38b13cf3c22f4e3364f1233907c
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2023-06-14 16:28:05 +0530

    added environment variables

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const port = 5000;
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 const User = require('./models/user');
 
-const url = "mongodb+srv://Ankush:ganya@learning.id5ibpg.mongodb.net/test?retryWrites=true&w=majority";
+const url = process.env.MONGODB_URI;
 
 mongoose.connect(url, (err) => {
     if (err) throw err;
